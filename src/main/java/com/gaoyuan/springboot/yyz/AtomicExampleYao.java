d1ff28556a198ed4a9b0783c571b146907a3eb75
yaoyizhou1 <yauri.zhang@qunar.com>
2019-05-31 16:07:53 +0800

    初始化

--- src/main/java/com/gaoyuan/springboot/yyz/AtomicExampleYao.java
+++ src/main/java/com/gaoyuan/springboot/yyz/AtomicExampleYao.java
@@ -1,34 +1,35 @@
 package com.gaoyuan.springboot.yyz;
 
-import com.gaoyuan.springboot.demo.annoations.NotThreadSafe;
+import com.gaoyuan.springboot.demo.annoations.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 功能描述:
  *
  * @author yaoyizhou
- * @date 2019/5/31 14:51
+ * @date 2019/5/31 15:56
  * @desc
  */
 @Slf4j
-@NotThreadSafe
-public class ConcurrencyYao {
-    // 请求总数
-    public static int clientTotal = 5000;
-    // 同时并发执行的线程数
-    public static int threadTotal = 200;
-    public static int count = 0;
+@ThreadSafe
+public class AtomicExampleYao {
+    private static final int threadNum = 5000;
+    private static final int semphoreNum = 50;
+    private static AtomicInteger atomicLong = new AtomicInteger(0);
 
     public static void main(String[] args) throws Exception {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
-        final Semaphore semaphore = new Semaphore(threadTotal);
-        for (int i = 0; i < clientTotal; i++) {
+        final Semaphore semaphore = new Semaphore(semphoreNum);
+        final CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+
+        for (int i = 0; i < threadNum; i++) {
             executorService.execute(() -> {
                 try {
                     semaphore.acquire();
@@ -42,10 +43,13 @@ public class ConcurrencyYao {
         }
         countDownLatch.await();
         executorService.shutdown();
-        log.info("{}", count);
+        log.info("{}", atomicLong.get());
+
     }
 
     private static void add() {
-        count++;
+        atomicLong.incrementAndGet();
     }
+
+
 }
